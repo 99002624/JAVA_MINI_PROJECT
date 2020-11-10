f693c0d4482692ee844e7cb2d1fb5fca7cb95b24
saikiran <devineni.saikiran@ltts.com>
2020-11-10 17:01:00 +0530

    add implementation floder

---
+++ implementations/SocketClient.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+@SuppressWarnings("serial")
+public class SocketClient extends JFrame implements ActionListener, Runnable {
+    JTextArea textArea = new JTextArea();
+    JScrollPane jp = new JScrollPane(textArea);
+    JTextField input_Text = new JTextField();
+    JMenuBar menuBar = new JMenuBar();
+
+    Socket sk;
+    BufferedReader br;
+    PrintWriter pw;
+
+    public SocketClient() {
+        super("Group chat application");
+        setFont(new Font("Arial Black", Font.PLAIN, 12));
+        setForeground(new Color(0, 0, 51));
+        setBackground(new Color(51, 0, 0));
+        textArea.setToolTipText("Chat History");
+        textArea.setForeground(new Color(50, 205, 50));
+        textArea.setEditable(false);
+        textArea.setFont(new Font("Monospaced", Font.BOLD, 13));
+
+        textArea.setBackground(new Color(0, 0, 0));
+      getContentPane().add(jp, "Center");
+        input_Text.setText("Enter your Message:");
+        input_Text.setToolTipText("Enter your Message");
+        input_Text.setForeground(new Color(0, 0, 0));
+        input_Text.setFont(new Font("Tahoma", Font.BOLD, 11));
+        input_Text.setBackground(new Color(230, 230, 250));
+        
+        getContentPane().add(input_Text, "South");
+        setSize(325, 411);
+        setVisible(true);
+
+        input_Text.requestFocus(); //Place cursor at run time, work after screen is shown
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        input_Text.addActionListener(this); //Event registration
+    }
+
+   
+}
